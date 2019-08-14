b84ae3e21305c7520339be6ef52caaaf4bb6fdc6
SimonRijsselaere <rijsselaere.simon@gmail.com>
2019-08-14 15:20:41 +0200

    remove router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './imagelink/test.component';
 import { ContactComponent } from './contact/contact.component';
@@ -19,7 +18,6 @@ import { GooglemapsComponent } from './googlemaps/googlemaps.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
