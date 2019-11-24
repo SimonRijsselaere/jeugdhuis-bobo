e6509c61a4b764e289f898dec76334b217a179ba
SimonRijsselaere <rijsselaere.simon@gmail.com>
2019-11-25 00:18:46 +0100

    redesign

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,14 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './imagelink/test.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
-import { GooglemapsComponent } from './googlemaps/googlemaps.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
     ContactComponent,
     FooterComponent,
-    GooglemapsComponent,
   ],
   imports: [
     BrowserModule,
